24f88652a08369503dfc5a46d1520397b7d676b7
Jack Belshe <jackbelshe@Jacks-Laptop.local>
2025-09-08 15:18:59 -0600

    switched to postcss.config.mjs from .js

---
+++ postcss.config.mjs
@@ -0,0 +1,14 @@
+// // postcss.config.js
+// module.exports = {
+//   plugins: {
+//     "@tailwindcss/postcss": {},
+//   },
+// };
+
+// postcss.config.mjs
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+export default config;
